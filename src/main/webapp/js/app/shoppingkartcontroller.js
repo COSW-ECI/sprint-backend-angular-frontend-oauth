be073e87ce4cefe838381efbdeba2d25cd9e1236
Hector <hector.cadavid@gmail.com>
2015-10-07 11:45:22 -0500

    multipleviews defined, no controllers yet

--- src/main/webapp/js/app/shoppingkartcontroller.js
+++ src/main/webapp/js/app/shoppingkartcontroller.js
@@ -1,6 +1,19 @@
 (function () {
     var app = angular.module('ShoppingKart', ['ngRoute']);
 
+    app.config(function ($routeProvider) {
+        $routeProvider
+                .when('/new', {
+                    templateUrl: 'seleccion.html'
+                })                
+                .when('/list', {
+                    templateUrl: 'checkout.html'
+                })
+                .when('/home', {
+                    templateUrl: 'home.html'                 
+                });
+    });
+
 
     app.controller('skcontroller', function ($scope,$location) {
 
